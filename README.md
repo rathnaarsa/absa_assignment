b3747612f76e532bd676412b680449b514eb9652
Rathnakar Arram <rathnakar.arrams@gmail.com>
2020-08-02 20:58:07 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # absa_assignment
 Build the project using the below mentioned command
 mvn clean package
+
+Objectives : This document provides a high level view of the type of test that is scheduled
+to be carried out and the features to be tested.
+Git URL for source code :
+https://github.com/rathnakararram/absa_assignment
+Technologies Required :
+Building Platform : Eclipse, MS-Excel
+Frameworks : POM(Page object model),TestNG
+Tools : Selenium,Log4j
+Language : Java
+Jar Files : jxl,log4j
+Browser : GoogleChrome
+Build Tool : Maven
+Drivers : Chrome Driver
+Operating System : Windows
--- src/main/java/com/absa/usermanagement/pages/HomePage.java
+++ src/main/java/com/absa/usermanagement/pages/HomePage.java
@@ -2,7 +2,6 @@ package com.absa.usermanagement.pages;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,10 +10,12 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage extends BasePage {
 	Logger log = Logger.getLogger(HomePage.class);
+
 	public HomePage(WebDriver driver, WebDriverWait wait) {
 		super(driver, wait);
 	}
-	//web element locators
+
+	// web element locators
 	By user = By.xpath("//button[@class='btn btn-link pull-right']");
 	By fname = By.name("FirstName");
 	By lname = By.name("LastName");
@@ -29,14 +30,16 @@ public class HomePage extends BasePage {
 	By tr = By.tagName("tr");
 	String row;
 
-	//getting the name of the title
+	// getting the name of the title
 	public String getHomePageTitle() {
 		return driver.getTitle();
 	}
-	//Entering the userdata from excel file
-	public void addUserData(String FirstName, String LastName, String UserName, String Password,String Company, String Role, String Mail, String Phone) {
+
+	// Entering the userdata from excel file
+	public void addUserData(String FirstName, String LastName, String UserName, String Password, String Company,
+			String Role, String Mail, String Phone) {
 		List<String> UserNames = webTableData(UserName);
-		if(!UserNames.contains(UserName)) {
+		if (!UserNames.contains(UserName)) {
 			forClick(user);
 			forClear(fname);
 			forSendKeys(fname, FirstName);
@@ -47,40 +50,41 @@ public class HomePage extends BasePage {
 			forClear(password);
 			forSendKeys(password, Password);
 			List<WebElement> radiobuttons = driver.findElements(By.name("radiobuttonname"));
-		    for(WebElement radiobutton: radiobuttons) { 
+			for (WebElement radiobutton : radiobuttons) {
 
-		    if(radiobutton.getAttribute("value").equals(Company))
-		        radiobutton.click();
-		}
-			forSelect(role,Role);
+				if (radiobutton.getAttribute("value").equals(Company))
+					radiobutton.click();
+			}
+			forSelect(role, Role);
 			forClear(email);
-			forSendKeys(email,Mail);
+			forSendKeys(email, Mail);
 			forClear(phone);
-			forSendKeys(phone,Phone);
+			forSendKeys(phone, Phone);
 			forClick(save);
-			log.info("user is added" +":"+ UserName);
+			log.info("user is added" + ":" + UserName);
 		} else {
 			log.info("User already exists");
 		}
 	}
+
 	/**
 	 * @param UserName
 	 * @return
 	 */
-	//For reading the webtable data from webpage and returning the username for verifying whether it is unique or not
+	// For reading the webtable data from webpage and returning the username for
+	// verifying whether it is unique or not
 	public List<String> webTableData(String UserName) {
 		WebElement tables = driver.findElement(table);
-		List<WebElement> tableRows =tables.findElements(tr);
-		int row_num=1,col_num=1;
+		List<WebElement> tableRows = tables.findElements(tr);
+		int row_num = 1, col_num = 1;
 		List<String> UserNames = new ArrayList<String>();
-		for(WebElement webElement:tableRows) {
-			List<WebElement> td_collection = webElement.findElements(By.tagName("td")); 
+		for (WebElement webElement : tableRows) {
+			List<WebElement> td_collection = webElement.findElements(By.tagName("td"));
 			int n = td_collection.size();
-			for(int i=0;i<n;i++)
-				col_num=1;
-			for (WebElement tdElement  :td_collection)
-			{
-				if(row_num==1 && col_num == 3 && tdElement.getText().equals(UserName)) {
+			for (int i = 0; i < n; i++)
+				col_num = 1;
+			for (WebElement tdElement : td_collection) {
+				if (row_num == 1 && col_num == 3 && tdElement.getText().equals(UserName)) {
 					UserNames.add(tdElement.getText());
 					System.out.println(UserNames + "##########");
 				}
